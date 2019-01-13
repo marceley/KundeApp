c68abeafd4f16034e88f7fea23d96f85b86d9b1c
Marc Eley <marc@eley.dk>
2019-01-13 13:28:34 +0100

    clean up firebase and bump version

--- src/environment.ts
+++ src/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
  production: false,
  firebase: {
-    apiKey: "AIzaSyANmu1NGnjIsjzsrQc7_53480gQuYhg5GA",
-    authDomain: "harvesttracker-c79ca.firebaseapp.com",
-    databaseURL: "https://harvesttracker-c79ca.firebaseio.com",
-    projectId: "harvesttracker-c79ca",
-    storageBucket: "harvesttracker-c79ca.appspot.com",
-    messagingSenderId: "570722597332"
- }
+   apiKey: "AIzaSyBGbxWAod0mjq7-1VENoUyfDZHNaexmHsg",
+   authDomain: "aarstiderne-96d75.firebaseapp.com",
+   databaseURL: "https://aarstiderne-96d75.firebaseio.com",
+   projectId: "aarstiderne-96d75",
+   storageBucket: "aarstiderne-96d75.appspot.com",
+   messagingSenderId: "387341916630"
+}
 };
